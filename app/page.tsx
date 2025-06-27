7e69fab55d1af7f0f6daeb4c30385be9b1937c00
Pablito <pahapasha11@gmail.com>
2025-06-27 14:01:04 +0700

    test ci/cd

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
 import '@/styles/global.scss'
 export default function Home() {
-	return <div>Test
-
-		<button className="btn">button</button>
-	</div>
+  return (
+    <div>
+      Test
+      <button className="btn">button</button>
+      <button className="btn-outline">button-outline</button>
+      <button className="btn-primary">button-primary</button>
+      <div className="card bg-primary m-2">
+        <div className="p-2">test</div>
+        <div className="bg-accent p-2">test</div>
+      </div>
+      <div className="p-2 flex flex-col gap-2">
+        <input className="input" />
+        <select className="select" />
+        <textarea className="textarea" />
+      </div>
+    </div>
+  )
 }
