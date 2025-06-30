d669100652f1ee067f12341544ce0ead01df99f5
Некрасов Павел <pahapasha11@gmail.com>
2025-06-30 10:27:00 +0700

    Edit page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
       <div className="p-2 flex flex-col gap-2">
         <input className="input" />
         <select className="select" />
+        <textarea className="textarea" />
       </div>
     </div>
   )
