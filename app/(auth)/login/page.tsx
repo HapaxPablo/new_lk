fd62152db99dddbdf3533f22ccef06d2810a0d22
Aleksandr Vladyko <mett0@bk.ru>
2025-07-03 20:54:40 +0700

    feat: работаю над layout and header

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 const page = async () => {
   return (
-    <div className="flex flex-col md:flex-row md:h-full md:gap-1">
+    <div className="flex flex-col md:flex-row md:h-full md:gap-1 w-full ">
       {/* Левая часть — форма */}
       <div className="flex flex-col items-center justify-center md:max-w-[420px] md:h-full md:justify-around w-full md:w-1/2">
         <div className="flex flex-col w-full gap-[34px]">
--- components/ui/Header/Header.tsx
+++ components/ui/Header/Header.tsx
@@ -1,19 +1,29 @@
+'use client'
 import Image from 'next/image'
 import styles from './Header.module.scss'
+import NavigationMenu from './naviMenu/NavigationMenu'
+import { useRouter } from 'next/navigation'
 
 export default function Header() {
+  const router = useRouter()
+  const onClickLogo = () => {
+    router.push('/about', { scroll: false })
+  }
   return (
     <div className={styles.header}>
-      <div className={styles.header__logo_wrapper}>
+      <div className={styles.header__logo_wrapper} onClick={onClickLogo}>
         <Image
           className={styles.header__logo}
           src="/logo.svg"
           alt="logo"
           width={120}
           height={24}
-          priority
+          priority={true}
+          title="логотип-rmc"
+          aria-label="logo"
         />
       </div>
+      <NavigationMenu />
     </div>
   )
 }
