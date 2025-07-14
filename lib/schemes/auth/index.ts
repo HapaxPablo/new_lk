63aa9544951fd31cf4f87df3d8d59e096a4b24b4
Pablito <pahapasha11@gmail.com>
2025-07-14 11:16:55 +0700

    fix imports

--- lib/schemes/auth/index.ts
+++ lib/schemes/auth/index.ts
@@ -1,11 +1,6 @@
 export { authSchema } from './auth.schema'
 export type { AuthSchema } from './auth.schema'
 
-export {
-  registerIndividualSchema,
-  registerLegalSchema,
-} from './register.schema'
-export type {
-  RegisterIndividualSchema,
-  RegisterLegalSchema,
-} from './register.schema'
+export type { RegisterSchema } from './register.schema'
+
+export { registerSchema } from './register.schema'
