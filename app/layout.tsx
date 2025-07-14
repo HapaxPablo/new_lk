d8de7e56f2bef3d7fa9465f0bd7a69d97e834414
Некрасов Павел <pahapasha11@gmail.com>
2025-07-14 11:13:37 +0700

    Edit layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import Footer from '@/components/ui/Footer/Footer'
-import Header from '@/components/ui/header/Header'
+import Header from '@/components/ui/Header/Header'
 import { metaDataConfigLayout } from '@/lib/configs/config-meta/configMetaData'
 import { AuthProvider } from '@/providers/auth-provider/AuthProvider'
 import { NotificationProvider } from '@/providers/notification/NotificationProvider'
