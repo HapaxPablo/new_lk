187575546b289fd24f39f0249426ce61a4b15ad2
Pablito <pahapasha11@gmail.com>
2025-08-19 18:02:27 +0700

    fix

--- components/ui/loader/LoaderSkeleton.tsx
+++ components/ui/loader/LoaderSkeleton.tsx
@@ -1,12 +1,16 @@
-export default function LoaderSkeleton() {
+interface LoaderSkeletonProps {
+  className?: string
+}
+
+export default function LoaderSkeleton({ className }: LoaderSkeletonProps) {
   return (
-    <div className="container mx-auto p-4">
+    <div className={`container mx-auto w-full p-4 ${className || ''}`}>
       <div className="animate-pulse">
-        <div className="h-8 bg-gray-200 rounded w-1/3 mb-4"></div>
+        <div className="h-8 bg-gray-400 rounded mb-4"></div>
         <div className="space-y-4">
-          <div className="h-4 bg-gray-200 rounded w-1/4"></div>
-          <div className="h-4 bg-gray-200 rounded w-1/2"></div>
-          <div className="h-4 bg-gray-200 rounded w-1/3"></div>
+          <div className="h-4 bg-gray-400 rounded"></div>
+          <div className="h-4 bg-gray-400 rounded "></div>
+          <div className="h-4 bg-gray-400 rounded "></div>
         </div>
       </div>
     </div>
