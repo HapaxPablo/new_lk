6b485aa09e0e3b3b52fd36156b28333c7f78e1a5
Pablito <pahapasha11@gmail.com>
2025-08-19 17:14:51 +0700

    fix test

--- app/(main)/nomenclatures/[id]/page.tsx
+++ app/(main)/nomenclatures/[id]/page.tsx
@@ -1,32 +1,64 @@
-
-import { ArticleDiv, Description, MapPlacement, ResponsibleCard, TabsWrapper } from "@/components/nomenclatureById";
-import Slider from "@/components/slider/Slider";
-import { Wrench, Radio, Megaphone, MapPin } from "lucide-react";
-
-
+import {
+  ArticleDiv,
+  Description,
+  MapPlacement,
+  ResponsibleCard,
+  TabsWrapper,
+} from '@/components/nomenclatureById'
+import Slider from '@/components/slider/Slider'
+import { Wrench, Radio, Megaphone, MapPin } from 'lucide-react'
 
 const page = async () => {
   return (
-    <div className="flex flex-row gap-1 p-12  w-full h-screen">
-      <div className="flex flex-col gap-2 w-2/5 h-full">
-        <div className="w-full rounded-md shadow-sm hover:shadow-md transition-shadow overflow-hidden">
-
-          <Slider width="100%" height="100%" autoPlay={true} autoPlayTime={1500} />
+    <div className="flex flex-row gap-1 p-4  w-full h-full">
+      <div className="flex flex-col gap-2 w-2/5 h-auto">
+        <div className="w-full h-[45%] rounded-md shadow-sm hover:shadow-md transition-shadow overflow-hidden">
+          <Slider
+            width="100%"
+            height="100%"
+            autoPlay={true}
+            autoPlayTime={1500}
+          />
         </div>
-        <div className="w-full h-full">
+        <div className="w-full h-auto">
           <Description />
         </div>
       </div>
 
-      <div className="flex flex-col w-full h-full bg-white overflow-y-auto rounded-md shadow-sm hover:shadow-md transition-shadow">
+      <div className="flex flex-col w-full bg-white overflow-y-auto rounded-md shadow-sm hover:shadow-md transition-shadow">
         <div className="flex flex-col gap-4 p-4">
-          <h2 className="text-xl font-semibold text-[#1E3961]">Ответственный</h2>
+          <h2 className="text-xl font-semibold text-[#1E3961]">
+            Ответственный
+          </h2>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full" style={{ gridTemplateRows: "repeat(2, 1fr)" }}>
-            <ResponsibleCard label="Бренд радио" icon={<Radio size={16} />} data="Михневич Н.С." color="bg-purple-100" />
-            <ResponsibleCard label="Техника" icon={<Wrench size={16} />} data="Шилин Е.А." color="bg-green-100" />
-            <ResponsibleCard label="Реклама" icon={<Megaphone size={16} />} data="Давыдова Вероника" color="bg-blue-100" />
-            <ResponsibleCard label="Размещение" icon={<MapPin size={16} />} data="60 000 ₽/мес." color="bg-red-100" />
+          <div
+            className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full"
+            style={{ gridTemplateRows: 'repeat(2, 1fr)' }}
+          >
+            <ResponsibleCard
+              label="Бренд радио"
+              icon={<Radio size={16} />}
+              data="Михневич Н.С."
+              color="bg-purple-100"
+            />
+            <ResponsibleCard
+              label="Техника"
+              icon={<Wrench size={16} />}
+              data="Шилин Е.А."
+              color="bg-green-100"
+            />
+            <ResponsibleCard
+              label="Реклама"
+              icon={<Megaphone size={16} />}
+              data="Давыдова Вероника"
+              color="bg-blue-100"
+            />
+            <ResponsibleCard
+              label="Размещение"
+              icon={<MapPin size={16} />}
+              data="60 000 ₽/мес."
+              color="bg-red-100"
+            />
           </div>
         </div>
         <hr className="solid m-4" />
@@ -35,17 +67,15 @@ const page = async () => {
             <h2 className="text-xl font-semibold text-[#1E3961]">
               Место вещания
             </h2>
-            <ArticleDiv article='5984' />
+            <ArticleDiv article="5984" />
           </div>
           <MapPlacement lat={56.011152} lng={92.814753} />
         </div>
         <hr className="solid m-4" />
-        <div className="h-full">
-          <TabsWrapper />
-        </div>
+        <TabsWrapper />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default page;
+export default page
--- components/nomenclatureById/map/Map.tsx
+++ components/nomenclatureById/map/Map.tsx
@@ -1,40 +1,65 @@
 'use client'
 
+import { useState } from 'react'
 import styles from './Map.module.scss'
 
-import { YMaps, Map as YMap, ZoomControl, Placemark } from '@pbe/react-yandex-maps'
+import {
+  YMaps,
+  Map as YMap,
+  ZoomControl,
+  Placemark,
+} from '@pbe/react-yandex-maps'
+import { Button } from '@/components/ui/button/Button'
 
 interface MapProps {
-    lng: number
-    lat: number
-    className?: string;
+  lng: number
+  lat: number
+  className?: string
 }
 
 export function MapPlacement({ className, lat, lng }: MapProps) {
-    const city = { lng: lng, lat: lat }
-    const zoom = 17;
+  const [showMap, setShowMap] = useState(false)
 
-    const isMobile = false
-
-
-    // Return a div with the ref attached
-    return (
+  return (
+    <>
+      {!showMap ? (
+        <Button
+          onClick={() => setShowMap(true)}
+          className="p-2 bg-blue-500 text-white rounded w-full"
+        >
+          Показать карту
+        </Button>
+      ) : (
         <div className={styles['map-wrapper']}>
-            <YMaps query={{ lang: 'ru_RU' }}>
-                <YMap
-                    defaultState={{
-                        center: [city.lat, city.lng],
-                        zoom: 17,
-                        behaviors: isMobile ? ['multiTouch'] : ['drag'],
-                    }}
-                    height="400px"
-                    width="100%"
-                >
-                    <ZoomControl />
-                    <Placemark geometry={[city.lat, city.lng]} properties={{ hintContent: "Муниципальное образование" }} options={{ preset: 'islands#redDotIconWithCaption', iconColor: '#FF0000' }} />
-                </YMap>
-            </YMaps>
+          <Button
+            onClick={() => setShowMap(false)}
+            className="p-2 bg-blue-500 text-white rounded w-full"
+          >
+            Скрыть карту
+          </Button>
+          <YMaps query={{ lang: 'ru_RU' }}>
+            <YMap
+              defaultState={{
+                center: [lat, lng],
+                zoom: 17,
+                behaviors: ['drag'],
+              }}
+              height="400px"
+              width="100%"
+            >
+              <ZoomControl />
+              <Placemark
+                geometry={[lat, lng]}
+                properties={{ hintContent: 'Муниципальное образование' }}
+                options={{
+                  preset: 'islands#redDotIconWithCaption',
+                  iconColor: '#FF0000',
+                }}
+              />
+            </YMap>
+          </YMaps>
         </div>
-
-    )
+      )}
+    </>
+  )
 }
