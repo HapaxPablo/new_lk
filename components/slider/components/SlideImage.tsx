bcd4958e8307971ba498ff4b1da990c8e2953e85
Pablito <pahapasha11@gmail.com>
2025-08-19 17:29:55 +0700

    fix

--- components/slider/components/SlideImage.tsx
+++ components/slider/components/SlideImage.tsx
@@ -1,22 +1,29 @@
-import React from "react";
+import React from 'react'
 
-import styles from '../Slider.module.scss';
-import Image from "next/image";
+import styles from '../Slider.module.scss'
+import Image from 'next/image'
 
-export default function SlideImage({ src, alt }: { src: string | null, alt: string }) {
-    if (!src || src.trim() === '') {
-        return null;
-    }
-    return (
-        <div className={styles.slider__list__slide__image}>
-            <Image
-                src={src}
-                alt={alt}
-                width={800}
-                height={600}
-                sizes="(max-width: 600px) 100vw, 600px"
-                loading="lazy"
-            />
-        </div>
-    );
+export default function SlideImage({
+  src,
+  alt,
+}: {
+  src: string | null
+  alt: string
+}) {
+  if (!src || src.trim() === '') {
+    return null
+  }
+  return (
+    <div className={styles.slider__list__slide__image}>
+      <Image
+        src={src}
+        alt={alt}
+        fill
+        sizes="(max-width: 800px) 100vw, 800px"
+        // loading="lazy"
+        style={{ objectFit: 'contain', objectPosition: 'center' }}
+        priority
+      />
+    </div>
+  )
 }
