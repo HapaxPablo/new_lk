d3c25b408420b06f605383c2a129fcb6bd561550
Aleksandr Vladyko <mett0@bk.ru>
2026-01-07 22:01:35 +0700

    fix: доработка компонента pageDevelop

--- components/page-development/PageDevelop.tsx
+++ components/page-development/PageDevelop.tsx
@@ -1,8 +1,17 @@
 'use client'
+
+import { NAV_ITEMS } from '@/lib/configs/configMenuHeader'
+import { log } from 'console'
+import { usePathname } from 'next/navigation'
+
 const PageDevelop = () => {
+  const path = usePathname()
+  console.log(path)
+  const title = NAV_ITEMS.find((item) => item.path === path)?.title
   return (
     <div className="flex flex-col justify-center items-center w-full h-full text-center">
-      <h1 >Страница находится в разработке</h1>
+      <h1>{title || 'Страница не определена'}</h1>
+      <h3>Страница находится в разработке</h3>
       <span>Мы активно работаем над ней, скоро она станет доступна!</span>
     </div>
   )
