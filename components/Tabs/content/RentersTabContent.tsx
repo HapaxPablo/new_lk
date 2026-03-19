81043217215251e27437a6a0daac21c32d960923
zabor <pahapasha11@gmail.com>
2026-03-19 10:54:11 +0700

    feat: список арендаторов с отображением логотипа

--- components/Tabs/content/RentersTabContent.tsx
+++ components/Tabs/content/RentersTabContent.tsx
@@ -1,15 +1,34 @@
 'use client'
 
-import React from 'react'
-
-interface Tenant {
-  id: string
-  name: string
-  brands_list: string
-}
+import { ITenantsListItem } from '@/types/nomenclature'
+import Image from 'next/image'
+import React, { useState } from 'react'
 
 interface RentersTabContentProps {
-  tenants: Tenant[]
+  tenants: ITenantsListItem[]
+}
+
+const TenantLogo = ({ tenant }: { tenant: ITenantsListItem }) => {
+  const [hasError, setHasError] = useState(false)
+  const [shouldRender, setShouldRender] = useState(
+    tenant.tenant.logotypes && tenant.tenant.logotypes.length > 0
+  )
+
+  // Если нет логотипа или была ошибка загрузки, не рендерим
+  if (!shouldRender || hasError) {
+    return null
+  }
+
+  return (
+    <Image
+      src={tenant.tenant.logotypes![0]}
+      alt={`${tenant.tenant.brands_list} логотип`}
+      width={50}
+      height={50}
+      className="object-contain mr-2"
+      onError={() => setHasError(true)} // При ошибке загрузки скрываем изображение
+    />
+  )
 }
 
 export const RentersTabContent = ({ tenants }: RentersTabContentProps) => {
@@ -20,7 +39,13 @@ export const RentersTabContent = ({ tenants }: RentersTabContentProps) => {
   return (
     <ul>
       {tenants.map((tenant, index) => (
-        <li key={tenant.id || index}>👥 {tenant.brands_list}</li>
+        <li
+          key={tenant.tenant.id || index}
+          className="flex items-center mb-2 flex-row"
+        >
+          <TenantLogo tenant={tenant} />
+          {tenant.tenant.brands_list}
+        </li>
       ))}
     </ul>
   )
