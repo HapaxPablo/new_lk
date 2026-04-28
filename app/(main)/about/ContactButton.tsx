4b13a8a2d540791c6ef0a87c0bceedff470f7262
zabor <pahapasha11@gmail.com>
2026-04-28 14:57:21 +0700

    feat: обновлен компонент ContactButton и стили кнопок для улучшения навигации

--- app/(main)/about/ContactButton.tsx
+++ app/(main)/about/ContactButton.tsx
@@ -1,18 +1,33 @@
-import { Button } from '@/components/ui/button/Button'
+'use client'
 
-export default function ContactButton({ className }: { className: string }) {
+import { Button } from '@/components/ui/button/Button'
+import { useRouter } from 'next/navigation'
+
+
+export default function ContactButton() {
+
+    const router = useRouter()
+
+    const handleNavigate = () => {
+        router.push('/nomenclatures')
+    }
+
+    const handleScrollToContact = () => {
+        document.getElementById('contact')?.scrollIntoView({ behavior: 'smooth' })
+    }
+
     return (
         <div className='flex flex-row gap-2 w-full items-center justify-center'>
-            <a href="#contact" className={className}>
-                <Button>
-                    Свяжитесь с нами
-                </Button>
-            </a>
-            <a href="/nomenclatures" className={className}>
-                <Button>
-                    Выберите места
-                </Button>
-            </a>
-        </div>
+
+            <Button onClick={handleScrollToContact}>
+                Свяжитесь с нами
+            </Button>
+
+
+            <Button variant='navigate' onClick={handleNavigate}>
+                Выберите места
+            </Button>
+
+        </div >
     )
 }
