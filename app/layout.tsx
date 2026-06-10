f50a52846170fd4a437897a0e3509979a7497c42
Zabor <pahapasha11@gmail.com>
2026-06-10 12:54:21 +0700

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -130,8 +130,20 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <meta charSet="UTF-8" />
-      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID!} />
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID!} />=
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID!}`}
+        strategy="afterInteractive"
+      />
 
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', ${process.env.NEXT_PUBLIC_GA_ID!});
+        `}
+      </Script>
       <head>
         <Script
           id="organization-jsonld"
